773a3fb6ead4dcde6dfecabf13b4cc363ef852ce
Muslim-Abubakr <ismusis@yandex.ru>
2023-03-05 00:34:03 +0300

    files added

---
+++ Practice.js
@@ -0,0 +1,32 @@
+/* let myVar = 11;
+
+// Only change code below this line
+myVar--; //myVar = myVar - 1;
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a = a - 6;
+b = b - 15;
+c = c - 1;
+
+a -= 6;
+b -= 15;
+c -= 1;
+*/
+
+// -------------------------------------
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+console.log(myStr);
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Practice assignments from the course
