2444ea76818c360388c8111109d024b6d3cdbe3c
Muslim-Abubakr <ismusis@yandex.ru>
2023-05-07 16:24:07 +0300

    added

--- Practice.js
+++ Practice.js
@@ -866,7 +866,7 @@ while (i >= 0) {
 }
 
 // Only change code below this line
-*/
+
 
 for(let num = 0; num < 5; num++) {
   console.log(num)
@@ -878,3 +878,23 @@ for (let i = 1; i < 6; i++) {
   myArray.push(i)
 }
 // Only change code below this line
+// Setup
+const myArray = [];
+for (let i = 1; i < 10; i += 2) {
+  myArray.push(i)
+}
+*/
+// Only change code below this line
+const myArr = [2, 3, 4, 5, 6];
+let total = 0
+for (let i = 0; i < myArr.length; i++) {
+  total = myArr[i]
+}
+console.log(total)
+
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+let total = 0
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i]
+}
