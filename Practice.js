ed32ef052f52d56a21b8965511045c1c793e6f25
Muslim-Abubakr <ismusis@yandex.ru>
2023-05-07 20:16:13 +0300

    added

--- Practice.js
+++ Practice.js
@@ -871,7 +871,8 @@ while (i >= 0) {
 for(let num = 0; num < 5; num++) {
   console.log(num)
 }
-
+]
+ m
 // Setup
 const myArray = [];
 for (let i = 1; i < 6; i++) {
@@ -898,3 +899,17 @@ let total = 0
 for (let i = 0; i < myArr.length; i++) {
   total += myArr[i]
 }
+
+function multiplyAll(arr) {
+  let product = 1;
+  // Only change code below this line
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    product = product * arr[i][j]
+  }
+}
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
