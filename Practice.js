b4a8f15baf164f0882f632453fe66f9348a0ff35
Muslim-Abubakr <ismusis@yandex.ru>
2023-05-24 15:05:12 +0300

    adad

--- Practice.js
+++ Practice.js
@@ -1195,3 +1195,17 @@ function testElseIf(val) {
 }
 
 testElseIf(7);
+
+function updateRecords(records, id, prop, value) {
+  if (value === "") {
+    delete records[id][prop];
+  } else if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && value !== "") {
+    if (records[id].hasOwnProperty("tracks") === false) {
+      records[id][prop] = [];
+    }
+    records[id][prop].push(value);
+  }
+  return records;
+}
