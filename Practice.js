3ce8f4fdfdefe28c6d0a5dccbe0da9bdef52faac
Muslim-Abubakr <ismusis@yandex.ru>
2023-05-31 21:37:58 +0300

    added

--- Practice.js
+++ Practice.js
@@ -1209,3 +1209,15 @@ function updateRecords(records, id, prop, value) {
   }
   return records;
 }
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const {today, tomorrow} = HIGH_TEMPERATURES;
+
+// Only change code above this line
