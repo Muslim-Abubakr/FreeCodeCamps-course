55a3f2e8b8fb9f5c9a06ecf2574a4f660f7697e7
Muslim-Abubakr <ismusis@yandex.ru>
2023-05-31 21:47:42 +0300

    added

--- Practice.js
+++ Practice.js
@@ -1233,3 +1233,15 @@ const HIGH_TEMPERATURES = {
 const {today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES
 
 // Only change code above this line
+
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
+  
+const {today: {high: highToday, low: lowToday}}= LOCAL_FORECAST
+
+// Only change code above this line
