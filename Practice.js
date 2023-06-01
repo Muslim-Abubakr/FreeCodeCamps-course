027b1f817aaf763a998722c0cf680612e96b84ec
Muslim-Abubakr <ismusis@yandex.ru>
2023-06-01 17:14:52 +0300

    added practice

--- Practice.js
+++ Practice.js
@@ -1248,3 +1248,11 @@ const {today: {high: highToday, low: lowToday}}= LOCAL_FORECAST
 
 let a = 8, b = 6;
  [a, b] = [b,a]
+
+ function removeFirstTwo(list) {
+    const[a, b, ...shorterList] = list
+    return shorterList
+ }
+
+ const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
