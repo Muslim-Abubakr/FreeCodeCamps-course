d7a490e6875af467051e9f50776dedc771a31614
Muslim-Abubakr <ismusis@yandex.ru>
2023-06-05 03:23:48 +0300

    added practice from freecodecourse

--- Practice.js
+++ Practice.js
@@ -1369,3 +1369,14 @@ stringFunctions.uppercaseString("hello");
 stringFunctions.lowercaseString("WORLD!");
 
 const makeServerRequest = new Promise((resolve, reject) => {})
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    resolve('We got the data')// Change this line
+  } else {  
+    reject('Data not received')// Change this line
+  }
+});
