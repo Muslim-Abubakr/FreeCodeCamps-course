713b7a0c0422b7ddcfefde0323d3d47ad87b5955
Muslim-Abubakr <ismusis@yandex.ru>
2023-06-05 03:18:29 +0300

    added practice from freecodecourse

--- Practice.js
+++ Practice.js
@@ -1361,3 +1361,9 @@ import {uppercaseString, lowercaseString} from './string_functions.js'
 
 uppercaseString("hello");
 lowercaseString("WORLD!");
+
+import * as stringFunctions from './string_functions.js'
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
