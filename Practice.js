ec3a23e7f16884e86cc9190bf17286a003f8dbf9
Muslim-Abubakr <ismusis@yandex.ru>
2023-06-05 16:26:53 +0300

    added

--- Practice.js
+++ Practice.js
@@ -1380,3 +1380,9 @@ const makeServerRequest = new Promise((resolve, reject) => {
     reject('Data not received')// Change this line
   }
 });
+
+const otherAngle = (a, b) => 180 - (a + b)
+
+function otherAngle(a, b) {
+  return 180-(a+b);
+}
