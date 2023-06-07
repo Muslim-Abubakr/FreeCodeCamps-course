f7f910282b6a3fd132e4f4e05e150bcc74b33aa2
Muslim-Abubakr <ismusis@yandex.ru>
2023-06-07 19:37:17 +0300

    added

--- Practice.js
+++ Practice.js
@@ -1399,3 +1399,7 @@ function multiTable(number) {
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
