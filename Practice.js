8f6d4e685c16b088dbd9728729ae191caa5e3f80
Muslim-Abubakr <ismusis@yandex.ru>
2023-06-27 17:44:58 +0300

    added

--- Practice.js
+++ Practice.js
@@ -1497,3 +1497,31 @@ const prices = [19.99, 12, 25, 3.50];
 let newPrices = prices.map(price => price * 1.06)
 
 console.log(newPrices)
+
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = 'Low'
+      break
+    case 4:
+    case 5:
+    case 6:
+      answer = 'Mid'
+      break
+    case 7:
+    case 8:
+    case 9:
+      answer = 'High'  
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+
